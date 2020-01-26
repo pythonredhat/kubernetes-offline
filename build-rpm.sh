d767bdccde0f0725ae17ec55c80b9da81cf6a904
pythonredhat <pythonredhat1987@gmail.com>
2020-01-26 16:43:58 -0500

    Add env variables

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#set build number
+if [ -z $BUILD_NUMBER ]; then 
+  echo BUILD_NUMBER is not known, it is normally given by Gitlab
+  exit 1
+fi
+
+RELEASE=$BUILD_NUMBER
+
+#set version
+if [ -z $VERSION ]; then
+  echo VERSION is not set via parameter or via ENV, using version.txt
+  VERSION=`cat version.txt`
+fi
+
 #build
 autoreconf -i
 ./configure
